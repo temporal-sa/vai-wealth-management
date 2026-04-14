1a4f16815b49f756506e18a387811f8f11f27e3d
Rick Ross <rick.ross@temporal.io>
2026-04-14 13:24:50 -0400

    Initial commit from @temporalio/create

---
+++ src/client.ts
@@ -0,0 +1,30 @@
+import { Connection, Client } from '@temporalio/client';
+import { loadClientConnectConfig } from '@temporalio/envconfig';
+import { nanoid } from 'nanoid';
+import { TASK_QUEUE_NAME } from './shared';
+
+// import your workflow
+import { YOUR_WORKFLOW } from './workflows';
+
+async function run() {
+  const config = loadClientConnectConfig();
+  const connection = await Connection.connect(config.connectionOptions);
+  const client = new Client({ connection });
+
+  const handle = await client.workflow.start(YOUR_WORKFLOW, {
+    taskQueue: TASK_QUEUE_NAME,
+    // type inference works! args: [name: string]
+    args: ['Temporal'],
+    // in practice, use a meaningful business ID, like customerId or transactionId
+    workflowId: 'workflow-' + nanoid(),
+  });
+  console.log(`Started workflow ${handle.workflowId}`);
+
+  // optional: wait for client result
+  console.log(await handle.result());
+}
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
